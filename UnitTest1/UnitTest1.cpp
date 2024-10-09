08dcca1bf29cac385d65decd04c3b00a58fa3ef9
Карина Печерная <karina.pecherna.03@gmail.com>
2024-10-09 13:13:49 +0300

    g

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab6.2/FileName.cpp" 
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+
+        TEST_METHOD(TestSwapWithLargestOdd)
+        {
+            const int SIZE = 5;
+            int arr[SIZE] = { 3, 7, -5, 10, 12 };  
+            SwapWithLargestOdd(arr, SIZE);
+
+            Assert::AreEqual(7, arr[SIZE - 1]);
+        }
+
+        
+    };
+}
---
+++ lab6.2/FileName.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void CreateArray(int* arr, const int size, const int low, const int high) {
+    for (int i = 0; i < size; i++) {
+        arr[i] = low + rand() % (high - low + 1);
+    }
+}
+
+
+void PrintArray(const int* arr, const int size) {
+    for (int i = 0; i < size; i++) {
+        cout << setw(4) << arr[i];
+    }
+    cout << endl;
+}
+
+
+void SwapWithLargestOdd(int* arr, const int size) {
+    int largestOdd = -1;
+    int largestOddIndex = -1;
+
+  
+    for (int i = 0; i < size; i++) {
+        if (arr[i] % 2 != 0 && arr[i] > largestOdd) {
+            largestOdd = arr[i];
+            largestOddIndex = i;
+        }
+    }
+
+   
+    if (largestOddIndex != -1) {
+        swap(arr[size - 1], arr[largestOddIndex]);
+    }
+}
+
+int main() {
+    const int SIZE = 10;
+    int arr[SIZE];
+    int low = -50, high = 50;
+
+   
+    CreateArray(arr, SIZE, low, high);
+    cout << "First: ";
+    PrintArray(arr, SIZE);
+
+    
+    SwapWithLargestOdd(arr, SIZE);
+
+    
+    cout << "Modified: ";
+    PrintArray(arr, SIZE);
+
+    return 0;
+}
+
+   
